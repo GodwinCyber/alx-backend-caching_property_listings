b9e1adc45b01f9f818a5fdec6f05387b507e5626
GodwinCyber <godwinchuks032@gmail.com>
2025-11-05 10:26:47 +0100

    added post_save and post_delete

---
+++ properties/signals.py
@@ -0,0 +1,19 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.core.cache import cache
+from .models import Property
+
+
+@receiver(post_save, sender=Property)
+def clear_cache_on_save(sender, instance, **kwargs):
+    '''Clear a property list created when a property us updated'''
+    cache.delete('all_properties')
+
+@receiver(post_delete, sender=Property)
+def clear_cache_on_delete(sender, instance, **kwargs):
+    '''Clear a property list cache when a property is deleted'''
+    cache.delete('all_properties')
+
+
+
+
